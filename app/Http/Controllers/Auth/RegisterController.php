715c1dc86e147d5dbae0777d17dedd9660046d47
八木修平 <yagishuhei@yagishuuheinoMacBook-Air.local>
2023-01-11 23:49:48 +0900

    パスワード再設定のリンクメール送信機能追加

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,9 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Password;
+
+use Log;
 
 class RegisterController extends Controller
 {
@@ -64,10 +67,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        $credentials['email'] = $data['email'];
+        Password::sendResetLink($credentials);
+        return $user;
     }
 }
