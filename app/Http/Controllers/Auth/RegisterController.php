bf7a3b2f737bbd5faaec5699e465ba8cd44c562c
八木修平 <yagishuhei@yagishuuheinoMacBook-Air.local>
2023-01-12 23:50:04 +0900

    [Add]roleカラムの追加

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Password;
 
 class RegisterController extends Controller
 {
@@ -51,8 +53,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users']
         ]);
     }
 
@@ -64,10 +65,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $password = Str::random(8);
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'role' => 'general',
+            'password' => Hash::make($password),
         ]);
+        $credentials['email'] = $data['email'];
+        Password::sendResetLink($credentials);
+        return $user;
     }
 }
