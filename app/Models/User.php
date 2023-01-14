cecae219fd1b3f7253ef1b879a0098897a82054b
八木修平 <yagishuhei@yagishuuheinoMacBook-Air.local>
2023-01-14 12:05:55 +0900

    [add]prettierの導入

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,22 +17,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'role',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'role'];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * The attributes that should be cast.
