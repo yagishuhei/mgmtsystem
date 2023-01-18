c450a970c5b375dbd1bc8a39c503addc365df7d7
八木修平 <yagishuhei@yagishuuheinoMacBook-Air.local>
2023-01-18 22:52:35 +0900

    [Fix]addressList

---
+++ resources/js/components/UserList.vue
@@ -0,0 +1,39 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>氏名</th>
+                <th>メールアドレス</th>
+                <th>登録日</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="user in users" :key="user.id">
+                <td>{{ user.id }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ user.created_at }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script>
+export default {
+    data: function () {
+        return {
+            users: [],
+        };
+    },
+    methods: {
+        getUsers() {
+            axios.get('/api/users').then((res) => {
+                this.users = res.data;
+            });
+        },
+    },
+    mounted() {
+        this.getUsers();
+    },
+};
+</script>
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/users', [App\Http\Controllers\UserController::class, 'index']);
 
 Route::get('/addresses', [
     App\Http\Controllers\AddressController::class,
